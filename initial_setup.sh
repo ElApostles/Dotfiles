fde510749e9895b9591c62786789ee30e12fb518
Hyeokjin Doo <hdoo@c8r2s4.42seoul.kr>
2022-10-04 11:13:44 +0900

    Add setting files

---
+++ initial_setup.sh
@@ -0,0 +1,6 @@
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
+tar xzf nvim-macos.tar.gz
+rm nvim-macos.tar.gz
+./auto_setup.sh
+brew install --cask --appdir=~/download iterm2
---
+++ zsh_setup.sh
@@ -0,0 +1,5 @@
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+curl https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/VictorMono/Medium/complete/Victor%20Mono%20Medium%20Nerd%20Font%20Complete.ttf --output victor_mono.ttf
+open victor_mono.ttf
+rm victor_mono.ttf
